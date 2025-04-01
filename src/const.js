e99b6922afa67554532cf3c1b4aa3ae1373cfc2a
Yroslav.Shikhov <slaykin72@gmail.com>
2025-04-01 13:35:31 +0500

    4.17. Шаблонизируй то

--- src/const.js
+++ src/const.js
@@ -10,4 +10,18 @@ const EVENT_TYPES = [
   'restaurant'
 ];
 
-export {EVENT_TYPES};
+const FilterType = {
+  EVERYTHING: 'everything',
+  FUTURE: 'future',
+  PRESENT: 'present',
+  PAST: 'past',
+};
+
+const NoEventsText = {
+  EVERYTHING: 'Click New Event to create your first point',
+  PAST: 'There are no past events now',
+  PRESENT: 'There are no present events now',
+  FUTURE: 'There are no future events now',
+};
+
+export {EVENT_TYPES, FilterType, NoEventsText};
--- src/view/point-list-empty-view.js
+++ src/view/point-list-empty-view.js
@@ -1,7 +1,12 @@
+import { NoEventsText } from '../const.js';
 import AbstractView from '../framework/view/abstract-view.js';
 
 function createPointListEmptyTemplate() {
-  return '<p class="trip-events__msg">Click New Event to create your first point</p>';
+  return (
+    `<p class="trip-events__msg">
+    ${NoEventsText.EVERYTHING}
+    </p>`
+  );
 }
 
 export default class PointListEmptyView extends AbstractView {
