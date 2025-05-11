90198370e69a78eec83a1ddaa4bceece1fa4a070
Yroslav.Shikhov <slaykin72@gmail.com>
2025-05-11 18:41:22 +0500

    Правки

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,28 +1,5 @@
 import {getCapitalizedString} from '../utils/common.js';
 import AbstractView from '../framework/view/abstract-view.js';
-import { capitalizeString } from '../utils.js';
-
-function createFilterItemTemplate(filter, isChecked) {
-  const {type, count} = filter;
-
-  return (
-    `<div class="trip-filters__filter">
-      <input id="filter-${type}"
-      class="trip-filters__filter-input  visually-hidden"
-      type="radio"
-      name="trip-filter"
-      value="${type}"
-      ${isChecked ? 'checked' : ''}
-      ${count === 0 ? 'disabled' : ''}>
-      <label class="trip-filters__filter-label" for="filter-${type}">${capitalizeString(type)}</label>
-    </div>`
-  );
-}
-
-function createFilterTemplate(filterItems) {
-  const filterItemsTemplate = filterItems
-    .map((filter, index) => createFilterItemTemplate(filter, index === 0))
-    .join('');
 
 function createFilterItemTemplate(filter, currentFilterType) {
   const {type, count} = filter;
